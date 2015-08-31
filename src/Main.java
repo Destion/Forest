1c1fc96e3c417305badc798c82edfd28bb461257
Destion <m.sneekes@student.utwente.nl>
2015-08-31 21:42:35 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Destion on 31-8-2015.
+ */
+public class Main {
+}
---
+++ src/entities/Lumberjack.java
@@ -0,0 +1,11 @@
+package entities;
+
+/**
+ * Created by Destion on 31-8-2015.
+ */
+public class Lumberjack {
+    private int movepoints;
+
+    private int lumber;
+
+}
